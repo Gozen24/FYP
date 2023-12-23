3b649a4bccca804fa59e66d90d7c111f0cb5fa27
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2023-12-23 22:58:29 +0800

    Update Dashboard.py
    update directory call

--- Dashboard.py
+++ Dashboard.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 
 # Load the trained GP model
 def DisplayGraph(state, name):
-    path = "C:/Users/Asus/Desktop/FYP/DATA/movavg/"
+    path = "FYP/movavg/"
     file = name
     format = ".csv"
     data = pd.read_csv(path + file + format)
@@ -24,7 +24,7 @@ def DisplayGraph(state, name):
     # Filter the data for the chosen state
     filtered_data = data[data['state'] == chosen_state]
 
-    model_filename = f"C:/Users/Asus/Desktop/FYP/DATA/savedModel/({chosen_state}){file}.pkl"
+    model_filename = f"FYP/savedModel/({chosen_state}){file}.pkl"
     try:
         # Try to load the model
         with open(model_filename, 'rb') as f:
@@ -82,7 +82,7 @@ def DisplayGraph(state, name):
     # Display the interactive plot using Streamlit
     st.plotly_chart(fig)
 
-path = "C:/Users/Asus/Desktop/FYP/DATA/movavg/"
+path = "FYP/movavg/"
 file = "COCA COLA (BOTOL),1.5 liter"
 format = ".csv"
 data = pd.read_csv(path + file + format)
