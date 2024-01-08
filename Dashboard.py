769856ce13e23188225aa45bf012d1d7ebf88b53
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2024-01-08 16:25:02 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -49,7 +49,7 @@ def SelectBox(category):
 # Load the trained GP model
 def DisplayGraph(state, name):
 
-    data = pd.read_csv("movavg/" + name + ".csv")
+    data = pd.read_csv("movavg/" + name)
     # data1 = pd.read_csv("C:/Users/Asus/Desktop/FYP/DATA/averaged/" + name + ".csv")
     # Convert date column to datetime format
     data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
