11fcb74fd2f221470dfc9efd55f9e3e01b386147
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2024-01-08 16:46:18 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -210,7 +210,7 @@ with tab1:
 
             # Display graph in the first column
             # with col1:
-            percentage_difference=DisplayGraph(chosen_state, name)
+            percentage_difference=DisplayGraph(chosen_state,category, name)
     with col1:
         
         if ((chosen_state != 'Choose a State...') and (name != 'Choose an item...')):
