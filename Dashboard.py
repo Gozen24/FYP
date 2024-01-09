cce24723667d70a73bbc6a02e6567c47816ba152
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2024-01-09 22:02:46 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
     col1,col2=st.columns([0.2,1])
     with col1:
         st.write("")
-        # st.image("dashboard/logo.png",width=100)
+        st.image("logo.png",width=100)
     with col2:
         st.title("HargaBarangNow")
         
