65cc0b1124eab18d5853e4c3b91a348daeeec8cd
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2024-01-12 19:13:18 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -19,7 +19,10 @@ def CurrentPrice(name,category):
     data['Percentage Difference (from OCT 2023)'] = data.groupby('state')['price'].pct_change() * 100
     # Filtering only the latest date prices
     df = data.groupby('state').tail(1)
-
+    
+    pos=df.nlargest(1,["Percentage Difference (from OCT 2023)"])
+    neg=df.nsmallest(1,["Percentage Difference (from OCT 2023)"])
+    
     df = df.rename(columns={'price': 'Price (NOV 2023)'})
     df = df.rename(columns={'state': 'State'})
     
@@ -33,6 +36,13 @@ def CurrentPrice(name,category):
     # st.write("Percentage difference of price between October 2023 and November 2023 ")
     st.dataframe(df[['State',  'Price (NOV 2023)', 'Percentage Difference (from OCT 2023)']],hide_index=True, use_container_width=True,height=600)
 
+    state_pos=pos['state'].iloc[0]
+    state_neg=neg['state'].iloc[0]
+    vary_pos=pos['Percentage Difference (from OCT 2023)'].iloc[0]
+    vary_neg=neg['Percentage Difference (from OCT 2023)'].iloc[0]
+
+    st.write(f"Most increment : :red[+{round(vary_pos,2)}%] in **{state_pos}** ")
+    st.write(f"Most decrement : :blue[{round(vary_neg,2)}%] in **{state_neg}** ")
 
 def Compare(name,category):
 
