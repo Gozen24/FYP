e86ca047757bef141f41b846061d99adca7a40fb
Gozen24 <154685823+Gozen24@users.noreply.github.com>
2024-09-13 10:23:59 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -99,7 +99,7 @@ def SelectBox(category):
     for file in csv_files:
         itemls.append(file[:-4])
         
-    name = st.selectbox("Item Name",sorted(itemls))
+    name = st.selectbox("Item Name",sorted(itemls), index=None,placeholder = "Select Item...")
     return name
     
 # Load the trained GP model
@@ -415,10 +415,10 @@ if __name__ == "__main__":
         
         col1, col2, col3 = st.columns([1,1,1])
         with col1:
-            chosen_state = st.selectbox('Select State', sorted(unique_states1), index=None,placeholder = "select state")
+            chosen_state = st.selectbox('Select State', sorted(unique_states1), index=None,placeholder = "Select State...")
         
         with col2:
-            category = st.selectbox('Select Food Category', ["Processed Food", "Raw Food"])
+            category = st.selectbox('Select Food Category', ["Processed Food", "Raw Food"], index=None,placeholder = "Select Category...")
             with col3:
                 if category is None:
                     name = st.selectbox('Select Item',["Select Category First"],disabled=True)
@@ -453,7 +453,7 @@ if __name__ == "__main__":
     
             col1, col2, col3 = st.columns([0.3,0.3,0.3])
             with col1:
-                category=st.selectbox("Food Categories",(['Processed Food','Raw Food']))
+                category=st.selectbox("Food Categories",(['Processed Food','Raw Food']), index=None,placeholder = "Select Category...")
                 
             with col2:
                 if category is None:
@@ -480,7 +480,7 @@ if __name__ == "__main__":
             st.subheader("Price List of Items For a Chosen Category in a State")
             col1,col2,col3=st.columns([0.7,0.7,1])
             with col1:
-                category=st.selectbox("Select Categories",(['Processed Food','Raw Food']))
+                category=st.selectbox("Select Categories",(['Processed Food','Raw Food']), index=None,placeholder = "Select Category...")
                 
             with col2:
                 if category is None:
